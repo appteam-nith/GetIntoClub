aefba41cba1ff2b1e4fb34728372132475536fcf
thisisnsh <hadanis.singh@gmail.com>
2018-08-28 21:39:09 +0530

    Initial Files Added

---
+++ app/src/main/java/com/nsh/getintoclub/SkillDetail.java
@@ -0,0 +1,17 @@
+package com.nsh.getintoclub;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class SkillDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skill_detail);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
